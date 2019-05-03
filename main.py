4d7762b8c067f6ae33f6db61e7a3b264a34fb8b2
birodb <bob@example.com>
2019-05-03 23:14:49 +0200

    pylint

--- main.py
+++ main.py
@@ -9,6 +9,20 @@ Example video plugin that is compatible with both Python 2 and 3
 Compatibility features are provided by ``script.module.future`` library addon.
 """
 
+import sys
+import json
+import re
+import os
+import time
+import urllib
+import datetime
+import xml.etree.ElementTree as ET
+
+import xbmc
+import xbmcaddon
+import xbmcplugin
+import xbmcgui
+
 # Enable unicode strings by default as in Python 3
 from __future__ import unicode_literals
 # Monkey-patch standard libary names to enable Python 3-like behavior
@@ -20,12 +34,7 @@ from future.utils import iterkeys
 # In Python 3 they do nothing and can be safely removed.
 # Normal imports for your addon:
 
-import sys
-import json
-import re
 
-import xbmcgui
-import xbmcplugin
 
 try:
     from html.parser import HTMLParser
@@ -84,19 +93,17 @@ class MyHTMLParser(HTMLParser):
     def handle_data(self, data):
         if self.curr_tag == "script":
             if "mtva_player_" in data:
-                for i in re.finditer('[^\{]*(\{[^;]+)\);', data):
+                for i in re.finditer('[^\\{]*(\\{[^;]+)\\);', data):
                     pl = json.loads(i.group(1))
                     self.player_data.append(pl)
                     #print("Encountered some data  :", json.dumps(pl))
             elif "pl.setup" in data:
-                for i in re.finditer('pl.setup\(\s*(\{[^;]+)\);', data):
+                for i in re.finditer('pl.setup\\(\\s*(\\{[^;]+)\\);', data):
                     setup_data = json.loads(i.group(1))
                     pl = setup_data.get('playlist', [])
                     self.stream_url = 'https:' + pl[1 if len(pl) > 1 else 0]['file']
                     #self.player_data.append(pl)
                     #print("Encountered some data  :", json.dumps(setup_data))
-                pass
-        pass
 
 def get_stream_url(url):
     #print(url)
@@ -125,139 +132,21 @@ def get_stream_url(url):
         print(e)
     return p.stream_url
 
-CHANNELS=[
-    {'name': 'DunaTV', 'id': 'dunalive', 'id2': 'duna-elo', 'num': '3' },
-    {'name': 'DunaWorld', 'id': 'dunaworldlive', 'id2': 'dunaworld-elo', 'num': '4' },
-    {'name': 'MTV1', 'id': 'mtv1live', 'id2': 'm1-elo', 'num': '1' },
-    {'name': 'MTV2', 'id': 'mtv2live', 'id2': 'm2-elo', 'num': '2' },
-    {'name': 'MTV4', 'id': 'mtv4live', 'id2': 'm4-elo', 'num': '30' },
-    {'name': 'MTV5', 'id': 'mtv5live', 'id2': 'm5-elo', 'num': '33' }
-]
-
+CHANNELS = [
+        {'name': 'DunaTV', 'id': 'dunalive', 'id2': 'duna-elo', 'num': '3'},
+        {'name': 'DunaWorld', 'id': 'dunaworldlive', 'id2': 'dunaworld-elo', 'num': '4'},
+        {'name': 'MTV1', 'id': 'mtv1live', 'id2': 'm1-elo', 'num': '1'},
+        {'name': 'MTV2', 'id': 'mtv2live', 'id2': 'm2-elo', 'num': '2'},
+        {'name': 'MTV4', 'id': 'mtv4live', 'id2': 'm4-elo', 'num': '30'},
+        {'name': 'MTV5', 'id': 'mtv5live', 'id2': 'm5-elo', 'num': '33'}
+        ]
 
 
 # Free sample videos are provided by www.vidsplay.com
 # Here we use a fixed set of properties simply for demonstrating purposes
 # In a "real life" plugin you will need to get info and links to video files/streams
 # from some web-site or online service.
-VIDEOS = {'Animals': [{'name': 'Crab',
-                       'thumb': 'http://www.vidsplay.com/wp-content/uploads/2017/04/crab-screenshot.jpg',
-                       'video': 'http://www.vidsplay.com/wp-content/uploads/2017/04/crab.mp4',
-                       'genre': 'Animals'},
-                      {'name': 'Alligator',
-                       'thumb': 'http://www.vidsplay.com/wp-content/uploads/2017/04/alligator-screenshot.jpg',
-                       'video': 'http://www.vidsplay.com/wp-content/uploads/2017/04/alligator.mp4',
-                       'genre': 'Animals'},
-                      {'name': 'Turtle',
-                       'thumb': 'http://www.vidsplay.com/wp-content/uploads/2017/04/turtle-screenshot.jpg',
-                       'video': 'http://www.vidsplay.com/wp-content/uploads/2017/04/turtle.mp4',
-                       'genre': 'Animals'}
-                      ],
-            'Cars': [{'name': 'Postal Truck',
-                      'thumb': 'http://www.vidsplay.com/wp-content/uploads/2017/05/us_postal-screenshot.jpg',
-                      'video': 'http://www.vidsplay.com/wp-content/uploads/2017/05/us_postal.mp4',
-                      'genre': 'Cars'},
-                     {'name': 'Traffic',
-                      'thumb': 'http://www.vidsplay.com/wp-content/uploads/2017/05/traffic1-screenshot.jpg',
-                      'video': 'http://www.vidsplay.com/wp-content/uploads/2017/05/traffic1.mp4',
-                      'genre': 'Cars'},
-                     {'name': 'Traffic Arrows',
-                      'thumb': 'http://www.vidsplay.com/wp-content/uploads/2017/05/traffic_arrows-screenshot.jpg',
-                      'video': 'http://www.vidsplay.com/wp-content/uploads/2017/05/traffic_arrows.mp4',
-                      'genre': 'Cars'}
-                     ],
-            'Food': [{'name': 'Chicken',
-                      'thumb': 'http://www.vidsplay.com/wp-content/uploads/2017/05/bbq_chicken-screenshot.jpg',
-                      'video': 'http://www.vidsplay.com/wp-content/uploads/2017/05/bbqchicken.mp4',
-                      'genre': 'Food'},
-                     {'name': 'Hamburger',
-                      'thumb': 'http://www.vidsplay.com/wp-content/uploads/2017/05/hamburger-screenshot.jpg',
-                      'video': 'http://www.vidsplay.com/wp-content/uploads/2017/05/hamburger.mp4',
-                      'genre': 'Food'},
-                     {'name': 'Pizza',
-                      'thumb': 'http://www.vidsplay.com/wp-content/uploads/2017/05/pizza-screenshot.jpg',
-                      'video': 'http://www.vidsplay.com/wp-content/uploads/2017/05/pizza.mp4',
-                      'genre': 'Food'}
-                     ]}
 
-
-def get_categories():
-    """
-    Get the list of video categories.
-
-    Here you can insert some parsing code that retrieves
-    the list of video categories (e.g. 'Movies', 'TV-shows', 'Documentaries' etc.)
-    from some site or server.
-
-    .. note:: Consider using `generator functions <https://wiki.python.org/moin/Generators>`_
-        instead of returning lists.
-
-    :return: The list of video categories
-    :rtype: types.GeneratorType
-    """
-    return iterkeys(VIDEOS)
-
-
-def get_videos(category):
-    """
-    Get the list of videofiles/streams.
-
-    Here you can insert some parsing code that retrieves
-    the list of video streams in the given category from some site or server.
-
-    .. note:: Consider using `generators functions <https://wiki.python.org/moin/Generators>`_
-        instead of returning lists.
-
-    :param category: Category name
-    :type category: str
-    :return: the list of videos in the category
-    :rtype: list
-    """
-    return VIDEOS[category]
-
-
-def list_categories_old():
-    """
-    Create the list of video categories in the Kodi interface.
-    """
-    # Set plugin category. It is displayed in some skins as the name
-    # of the current section.
-    xbmcplugin.setPluginCategory(_handle, 'My Video Collection')
-    # Set plugin content. It allows Kodi to select appropriate views
-    # for this type of content.
-    xbmcplugin.setContent(_handle, 'videos')
-    # Get video categories
-    categories = get_categories()
-    # Iterate through categories
-    for category in categories:
-        # Create a list item with a text label and a thumbnail image.
-        list_item = xbmcgui.ListItem(label=category)
-        # Set graphics (thumbnail, fanart, banner, poster, landscape etc.) for the list item.
-        # Here we use the same image for all items for simplicity's sake.
-        # In a real-life plugin you need to set each image accordingly.
-        list_item.setArt({'thumb': VIDEOS[category][0]['thumb'],
-                          'icon': VIDEOS[category][0]['thumb'],
-                          'fanart': VIDEOS[category][0]['thumb']})
-        # Set additional info for the list item.
-        # Here we use a category name for both properties for for simplicity's sake.
-        # setInfo allows to set various information for an item.
-        # For available properties see the following link:
-        # https://codedocs.xyz/xbmc/xbmc/group__python__xbmcgui__listitem.html#ga0b71166869bda87ad744942888fb5f14
-        # 'mediatype' is needed for a skin to display info for this ListItem correctly.
-        list_item.setInfo('video', {'title': category,
-                                    'genre': category,
-                                    'mediatype': 'video'})
-        # Create a URL for a plugin recursive call.
-        # Example: plugin://plugin.video.example/?action=listing&category=Animals
-        url = get_url(action='listing', category=category)
-        # is_folder = True means that this item opens a sub-list of lower level items.
-        is_folder = True
-        # Add our item to the Kodi virtual folder listing.
-        xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
-    # Add a sort method for the virtual folder items (alphabetically, ignore articles)
-    xbmcplugin.addSortMethod(_handle, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
-    # Finish creating a virtual folder.
-    xbmcplugin.endOfDirectory(_handle)
 
 def list_categories():
     """
@@ -271,7 +160,7 @@ def list_categories():
     xbmcplugin.setContent(_handle, 'videos')
     # Iterate through categories
     for channel in CHANNELS:
-    	add_live_tv(channel)
+        add_live_tv(channel)
     # Add a sort method for the virtual folder items (alphabetically, ignore articles)
     # xbmcplugin.addSortMethod(_handle, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
     # Finish creating a virtual folder.
@@ -292,7 +181,7 @@ def list_videos(category):
     # for this type of content.
     xbmcplugin.setContent(_handle, 'videos')
     # Get the list of videos in the category.
-    videos = get_videos(category)
+    videos = {} # get_videos(category)
     # Iterate through videos.
     for video in videos:
         # Create a list item with a text label and a thumbnail image.
@@ -364,20 +253,6 @@ def router(paramstring):
         # If the plugin is called from Kodi UI without any parameters,
         # display the list of video categories
         list_categories()
-    
-import os
-import time
-import urllib
-import xbmc
-import xbmcaddon
-import xbmcplugin
-import xbmcgui
-import json
-import re
-import sys
-import datetime
-import xml.etree.ElementTree as ET
-
 
 # setup
 cs_name = xbmcaddon.Addon().getAddonInfo("id")
@@ -387,11 +262,10 @@ cs_delay = 5
 class CsatornakURLopener(urllib.FancyURLopener):
     version = cs_agent
 
-	
 urllib._urlopener = CsatornakURLopener()
 def load_page(pg_url):
     # load and parse stream
-    print "Loading: " + pg_url
+    # print("Loading: " + pg_url)
     page = urllib.urlopen(pg_url)
     page_content = page.read()
     page.close()
@@ -406,71 +280,70 @@ def add_live_tv(c):
     cnt = 0
     for item in root.iter('Item'):
         start_date = None
-        date_xml =  item.find('Date')
+        date_xml = item.find('Date')
         if date_xml is not None and date_xml.text is not None:
             start_date = datetime.datetime(*(time.strptime(date_xml.text, '%Y-%m-%d %H:%M:%S')[0:6]))
         length_xml = item.find('Length')
         play_dt = None
         if length_xml is not None and length_xml.text is not None:
             t = time.strptime(length_xml.text, '%H:%M:%S')
-            play_dt = datetime.timedelta(hours = t.tm_hour, minutes = t.tm_min, seconds = t.tm_sec)
+            play_dt = datetime.timedelta(hours=t.tm_hour, minutes=t.tm_min, seconds=t.tm_sec)
         #if start_date > now:
         #    continue
         if now - start_date > play_dt:
             continue
-        if True:
-            title_xml = item.find('SeriesTitle')
-            if title_xml is None or title_xml.text is None:
-                title_xml = item.find('Title')
-            title = str(title_xml.text.encode('utf-8'))
-            descr_xml = item.find('Description')
-            description = None
-            if descr_xml is not None and descr_xml.text is not None:
-                description = str(descr_xml.text.encode('utf-8'))
-            #title += '.'
-            #for child in item:
-            #    if child.text:
-            #        s +=  child.tag + ': ' + str(child.text.encode('utf-8')) +'; '
-            #xbmcgui.Dialog().ok(cs_name, s, json.dumps(sys.argv), str(today))
-            play_min, play_sec = divmod(play_dt.seconds, 60)
-            if play_min:
-                name = '[{0}] {1} ({2}, {3}perc)'.format(c['name'], title, start_date.strftime('%H:%M'), play_min)
-            else:
-                name = '[{0}] {1} ({2}, {3:02}mp)'.format(c['name'], title, start_date.strftime('%H:%M'), play_sec)
-			
-			# Create a list item with a text label and a thumbnail image.
-			list_item = xbmcgui.ListItem(label=name)
-			# Set graphics (thumbnail, fanart, banner, poster, landscape etc.) for the list item.
-			# Here we use the same image for all items for simplicity's sake.
-			# In a real-life plugin you need to set each image accordingly.
-			#list_item.setArt({'thumb': VIDEOS[category][0]['thumb'],
-			#				  'icon': VIDEOS[category][0]['thumb'],
-			#				  'fanart': VIDEOS[category][0]['thumb']})
-			# Set additional info for the list item.
-			# Here we use a category name for both properties for for simplicity's sake.
-			# setInfo allows to set various information for an item.
-			# For available properties see the following link:
-			# https://codedocs.xyz/xbmc/xbmc/group__python__xbmcgui__listitem.html#ga0b71166869bda87ad744942888fb5f14
-			# 'mediatype' is needed for a skin to display info for this ListItem correctly.
-            list_item.setInfo('video', {'title': name, 'duration': play_dt.seconds, 'plot': description, 'mediatype': 'video'})
-			# Create a URL for a plugin recursive call.
-			# Example: plugin://plugin.video.example/?action=listing&category=Animals
-			list_item.setProperty('IsPlayable', 'true')
-			# Create a URL for a plugin recursive call.
-			# Example: plugin://plugin.video.example/?action=play&video=https://www.vidsplay.com/wp-content/uploads/2017/04/crab.mp4
-			url = get_url(action='play', video='https://www.mediaklikk.hu/{}/'.format(c['id2']))
-			# Add the list item to a virtual Kodi folder.
-			# is_folder = False means that this item won't open any sub-list.
-			is_folder = False
-			# Add our item to the Kodi virtual folder listing.
-			xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
+        title_xml = item.find('SeriesTitle')
+        if title_xml is None or title_xml.text is None:
+            title_xml = item.find('Title')
+        title = str(title_xml.text.encode('utf-8'))
+        descr_xml = item.find('Description')
+        description = None
+        if descr_xml is not None and descr_xml.text is not None:
+            description = str(descr_xml.text.encode('utf-8'))
+        #title += '.'
+        #for child in item:
+        #    if child.text:
+        #        s +=  child.tag + ': ' + str(child.text.encode('utf-8')) +'; '
+        #xbmcgui.Dialog().ok(cs_name, s, json.dumps(sys.argv), str(today))
+        play_min, play_sec = divmod(play_dt.seconds, 60)
+        if play_min:
+            name = '[{0}] {1} ({2}, {3}perc)'.format(c['name'], title, start_date.strftime('%H:%M'), play_min)
+        else:
+            name = '[{0}] {1} ({2}, {3:02}mp)'.format(c['name'], title, start_date.strftime('%H:%M'), play_sec)
 
-			cnt = cnt + 1
-            if cnt > 2:
-                break
+        # Create a list item with a text label and a thumbnail image.
+        list_item = xbmcgui.ListItem(label=name)
+        # Set graphics (thumbnail, fanart, banner, poster, landscape etc.) for the list item.
+        # Here we use the same image for all items for simplicity's sake.
+        # In a real-life plugin you need to set each image accordingly.
+        #list_item.setArt({'thumb': VIDEOS[category][0]['thumb'],
+        #				  'icon': VIDEOS[category][0]['thumb'],
+        #				  'fanart': VIDEOS[category][0]['thumb']})
+        # Set additional info for the list item.
+        # Here we use a category name for both properties for for simplicity's sake.
+        # setInfo allows to set various information for an item.
+        # For available properties see the following link:
+        # https://codedocs.xyz/xbmc/xbmc/group__python__xbmcgui__listitem.html#ga0b71166869bda87ad744942888fb5f14
+        # 'mediatype' is needed for a skin to display info for this ListItem correctly.
+        list_item.setInfo('video', {'title': name, 'duration': play_dt.seconds, 'plot': description, 'mediatype': 'video'})
+        # Create a URL for a plugin recursive call.
+        # Example: plugin://plugin.video.example/?action=listing&category=Animals
+        list_item.setProperty('IsPlayable', 'true')
+        # Create a URL for a plugin recursive call.
+        # Example: plugin://plugin.video.example/?action=play&video=https://www.vidsplay.com/wp-content/uploads/2017/04/crab.mp4
+        url = get_url(action='play', video='https://www.mediaklikk.hu/{}/'.format(c['id2']))
+        # Add the list item to a virtual Kodi folder.
+        # is_folder = False means that this item won't open any sub-list.
+        is_folder = False
+        # Add our item to the Kodi virtual folder listing.
+        xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
 
-    
-    #liz = xbmcgui.ListItem(' '.join((cs_name, json.dumps(sys.argv), str(date.today())))))#'XBMC list Example Title')
+        cnt = cnt + 1
+        if cnt > 2:
+            break
+
+
+#liz = xbmcgui.ListItem(' '.join((cs_name, json.dumps(sys.argv), str(date.today())))))#'XBMC list Example Title')
 
 # play stream
 #xbmcgui.Dialog().ok(cs_name, json.dumps(sys.argv), str(date.today()))
@@ -536,20 +409,20 @@ if __name__ == '__main__':
     # Call the router function and pass the plugin call parameters to it.
     # We use string slicing to trim the leading '?' from the plugin call paramstring
     router(sys.argv[2][1:])
-	#print "Session: " + cs_file
+    #print "Session: " + cs_file
 
-	# tries to limit deadlocks due to multiple runs
-	# fcntl for file locking may not be available on some platforms
-	#if os.path.exists(cs_file) and time.time() - os.path.getmtime(cs_file) < cs_delay:
-	#	print "Recently ran, exiting."
-	#	sys.exit(0)
-	#else:
-	#	with open(cs_file, "a+"):
-	#	os.utime(cs_file, None)
+    # tries to limit deadlocks due to multiple runs
+    # fcntl for file locking may not be available on some platforms
+    #if os.path.exists(cs_file) and time.time() - os.path.getmtime(cs_file) < cs_delay:
+    #	print "Recently ran, exiting."
+    #	sys.exit(0)
+    #else:
+    #	with open(cs_file, "a+"):
+    #	os.utime(cs_file, None)
 
-	#cs_player = xbmc.Player()
+    #cs_player = xbmc.Player()
 
-	# stop playing and wait
-	#cs_player.stop()
-	#while cs_player.isPlaying():
-	#	xbmc.sleep(10)
+    # stop playing and wait
+    #cs_player.stop()
+    #while cs_player.isPlaying():
+    #	xbmc.sleep(10)
